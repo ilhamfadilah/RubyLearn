e58924f996862f440d621e44b81d9bb4af101fae
ilhamfadilah <Dacim3d@gmail.com>
2020-02-05 12:04:27 +0700

    master ruby.1

---
+++ Putsandprint.rb
@@ -0,0 +1,16 @@
+# Puts & Print
+print "Kata Pertama\n"
+print "Kata Kedua\n"
+puts "Hallo Dunia\n"
+print "Kata Ketiga\n"
+
+# Interpolition
+a = 14
+b = 2
+puts "Penambahan #{a + b}"
+puts "Penambahan #{a - b}"
+puts "Penambahan #{a / b}"
+puts "Penambahan #{a % b} "
+puts "Penambahan #{a * b} \n\n"
+print "Tipe data a = " 
+puts a.class
---
+++ math.rb
@@ -0,0 +1,25 @@
+# Pemdas
+# parenteses ()
+# Multipication **
+# Division *
+# Addition / + -
+
+puts 1 - 2 + 3 * 4
+puts 1 + 2 - 3 /4.0
+puts 1 * ( 2 - 3 ) / 4.0
+puts 1 ** 2 - ( 3/4.0)
+
+puts 15.even?
+puts 18.even?
+puts 15.odd?
+puts 18.odd?
+
+
+# chomp = to string
+puts "Tolong Isi Nama Anda"
+name = gets.chomp
+print "Tolong Isi Password \n"
+pass = gets.to_i
+print "Tolong Isi Number"
+pass = gets.to_i
+puts "name : #{name} , pass : #{pass}"
